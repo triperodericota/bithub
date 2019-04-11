c0d634d191e2149333fe93abd2477c1ac58df5e4
Nicolas Mc Cormack <nicolasmccormack22@gmail.com>
2019-04-10 22:00:44 -0300

    project files. README.md edited

---
+++ createDatabase.sh
@@ -0,0 +1,1 @@
+mysql -uroot -pnico94322 -e "drop database if exists bd2_grupo20; create database bd2_grupo20;"
---
+++ src/main/java/ar/edu/unlp/info/bd2/config/AppConfig.java
@@ -0,0 +1,24 @@
+package ar.edu.unlp.info.bd2.config;
+
+import ar.edu.unlp.info.bd2.repositories.BithubRepository;
+import ar.edu.unlp.info.bd2.services.BithubService;
+import ar.edu.unlp.info.bd2.services.BithubServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+  @Bean
+  public BithubService createService() {
+    BithubRepository repository = this.createRepository();
+    return new BithubServiceImpl(repository);
+  }
+
+  @Bean
+  public BithubRepository createRepository() {
+    return new BithubRepository();
+  }
+
+}
+
