e12baab3dcd2272e11ff63ec86b4d0db1c05cb43
Ivana.Andrade <ivana.andrade96@gmail.com>
2019-04-24 16:29:53 -0300

    Script modificado

--- createDatabase.sh
+++ createDatabase.sh
@@ -1,1 +1,2 @@
-mysql -uroot -e "drop database if exists bd2_grupo20; create database bd2_grupo20;"
+mysql -uroot -e "drop database if exists bd2_grupo20; create database bd2_grupo20; drop user if exists 'cliente'@'localhost'; create user 'cliente'@'localhost' identified by 'bd2'; grant all privileges on *.* TO 'cliente'@'localhost';"
+ 
--- src/main/java/ar/edu/unlp/info/bd2/models/Commit.java
+++ src/main/java/ar/edu/unlp/info/bd2/models/Commit.java
@@ -1,7 +1,5 @@
 package ar.edu.unlp.info.bd2.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import javax.persistence.*;
 import java.util.List;
 
