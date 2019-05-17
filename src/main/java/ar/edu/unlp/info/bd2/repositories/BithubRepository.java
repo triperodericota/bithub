403f261e2d7223fd99b44567e91a8d39b242cba9
Nicolas Mc Cormack <nicolasmccormack22@gmail.com>
2019-05-17 19:45:21 -0300

    bithub service methods}

--- src/main/java/ar/edu/unlp/info/bd2/repositories/BithubRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/BithubRepository.java
@@ -1,11 +1,18 @@
 package ar.edu.unlp.info.bd2.repositories;
 
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import ar.edu.unlp.info.bd2.models.*;
- // import org.hibernate.SessionFactory; --> no se si tenemos que usar esta clase y pedirle getCurrentSession o trabajar con LocalSessionFactoryBean
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+// import org.hibernate.SessionFactory; --> no se si tenemos que usar esta clase y pedirle getCurrentSession o trabajar con LocalSessionFactoryBean
+
+@Transactional
 @Repository
 public class BithubRepository {
 
@@ -16,22 +23,40 @@ public class BithubRepository {
 
     }
 
-    public void createUser(User aNewUser){
-        session.getObject().openSession().save(aNewUser);
+    private Session getSession(){
+        return session.getObject().openSession();
+    }
+
+    public void createUser(User aNewUser){ this.getSession().save(aNewUser); }
+
+    public Optional<User> getUserByEmail(String email){
+/*        Query query = this.getSession()
+                .createQuery("FROM  User u " + " WHERE u.email like :email");
+
+        query.setParameter("email", email);
+        Optional<User> returned = Optional.of(query.getSingleResult());
+        return returned;*/
+        return Optional.empty();
     }
 
     public void createBranch(Branch aNewBranch){
-        session.getObject().openSession().save(aNewBranch);
+        this.getSession().save(aNewBranch);
     }
 
     public void createCommit(Commit aNewCommit){
-        session.getObject().openSession().save(aNewCommit);
+        this.getSession().save(aNewCommit);
     }
 
     public void createFile(File aNewFile){
-        session.getObject().openSession().save(aNewFile);
+        this.getSession().save(aNewFile);
     }
 
+    public Optional<Commit> getCommitByHash(String commitHash){
+        Commit c = (Commit) this.getSession().createQuery("select c " + "from Commit c " + "where c.hash like :commitHash").
+                setParameter("commitHash", commitHash).getSingleResult();
+        Optional<Commit> toReturn = Optional.of(c);
+        return toReturn;
+    }
 
 
 }
